17905af804c8381da5765a88ccb76c114d69eba9
Tim Hawes <trhawes@gmail.com>
2019-08-22 09:22:28 -0400

    Added pinterest with google login

---
+++ pinterest/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+nativefier --name "Pinterest" \
+           --icon pinterest.png \
+           --internal-urls "(google|pinterest)" \
+           --single-instance "https://www.pinterest.com/"
