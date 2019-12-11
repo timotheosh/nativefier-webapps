a37d82141621bf3dfcc328569c917f2270f9db6c
Tim Hawes <tim.hawes@genesys.com>
2019-12-11 10:06:13 -0500

    Added missing icons and desktop files. Set perms on icons and desktop files to 0644.

--- outlook/build.sh
+++ outlook/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 nativefier --name "Outlook" \
+   --icon outlook/outlook.png \
    --internal-urls "(genesys|microsoftonline|office)" \
    --single-instance "https://outlook.office365.com/mail/inbox"
