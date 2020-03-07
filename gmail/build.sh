3d75967fb464e25d7ba93e655cf813a8dae72932
Tim Hawes <trhawes@gmail.com>
2020-03-07 18:30:54 -0500

    Updated gmail app

--- gmail/build.sh
+++ gmail/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 nativefier --name "Gmail" \
-   --internal-urls "(contacts)" \
-   --single-instance "https://mail.google.com"
+           --user-agent 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0' \
+           --internal-urls ".*accounts .google.com.*" \
+           --single-instance "https://mail.google.com/mail"
