c616965ba556c509d644a41ed390b9f8c594b022
Tim Hawes <trhawes@gmail.com>
2020-06-17 11:40:33 -0400

    added lastfm

---
+++ lastfm/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+nativefier --name "LastFM" \
+    --icon lastfm.png \
+    --tray start-in-tray \
+    --single-instance "https://last.fm"
--- outlook/build.sh
+++ outlook/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 nativefier --name "Outlook" \
-   --icon outlook/outlook.png \
+   --icon outlook.png \
    --internal-urls "(genesys|microsoftonline|office)" \
    --single-instance "https://outlook.office365.com/mail/inbox"
