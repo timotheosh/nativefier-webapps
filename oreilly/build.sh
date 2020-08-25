c4a3a6b8db296b77b8e0833080658ab035a2656c
Tim Hawes <trhawes@gmail.com>
2020-08-25 17:21:43 -0400

    Update oreilly app

--- oreilly/build.sh
+++ oreilly/build.sh
@@ -3,4 +3,5 @@
 nativefier --name "Oreilly" \
            --show-menu-bar \
            --icon oreilly.png \
+           --internal-urls "(oreilly.com|on24.com)" \
            --single-instance "https://learning.oreilly.com/home/"
