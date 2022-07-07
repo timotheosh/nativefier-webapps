ce2c3abf9aadb6507abe9a1a84591cb3e003b804
Tim Hawes <trhawes@gmail.com>
2022-07-07 16:16:55 -0400

    Fix url for Outlook

--- outlook/build.sh
+++ outlook/build.sh
@@ -4,4 +4,4 @@ nativefier --name "Outlook" \
    --icon outlook.png \
    --tray start-in-tray \
    --internal-urls "(genesys|microsoftonline|office)" \
-   --single-instance "https://outlook.office365.com/mail/inbox"
+   --single-instance "https://outlook.office365.com/"
