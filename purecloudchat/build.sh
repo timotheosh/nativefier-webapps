957c98711fb082a0443ae97c932c80be6c91cc8a
Tim Hawes <trhawes@gmail.com>
2022-08-03 08:31:50 -0400

    Update purecloud client

--- purecloudchat/build.sh
+++ purecloudchat/build.sh
@@ -4,4 +4,4 @@ nativefier --name "PureCloudChat" \
     --icon purecloud.png \
     --tray start-in-tray \
     --internal-urls "mypurecloud.com" \
-    --single-instance "https://apps.mypurecloud.com/directory/"
+    --single-instance "https://apps.mypurecloud.com/"
