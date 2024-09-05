7310d70471bc81cfb71409fbfcd7c9faf2d9c00b
Tim Hawes <trhawes@gmail.com>
2024-09-04 20:11:30 -0400

    Update logos script

--- logos/build.sh
+++ logos/build.sh
@@ -3,4 +3,5 @@
 nativefier --name "Logos" \
            --icon logos.png \
            --tray start-in-tray \
+	   --internal-urls "(ref.ly|faithlife|faithlifetv)" \
            --single-instance "https://app.logos.com"
